0ecd2e34eb8a43dbfea2d7fd87f1b47ed0c9216d
Jan Szczypiński <jan.szczypinski@gmail.com>
2026-09-03 14:15:21 +0200

    Adding global.R

--- app.R
+++ app.R
@@ -4,23 +4,8 @@ library(DBI)
 library(duckdb)
 library(here)
 
-# Connect to database at startup
-con <- dbConnect(
-    duckdb(),
-    dbdir = here("data", "occurence.duckdb"),
-    read_only = TRUE
-)
-
-
-# source all in modules dir
-module_files <- list.files(
-    path = "modules",
-    pattern = "\\.R$",
-    full.names = TRUE,
-    ignore.case = TRUE
-)
-
-invisible(lapply(module_files, source))
+source("global.R")
+invisible(do.call(lapply, list(module_files, source)))
 source("lib/get_species_names.R")
 source("ui.R")
 source("server.R")
