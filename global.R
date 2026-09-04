37671b9ad06db56fd4a40407cc8f6cb1ebaaaead
Jan Szczypiński <jan.szczypinski@gmail.com>
2026-09-04 15:58:51 +0200

    Move listing modules files to app.R

--- global.R
+++ global.R
@@ -4,12 +4,3 @@ con <- dbConnect(
     dbdir = here("data", "occurence.duckdb"),
     read_only = TRUE
 )
-
-
-# list all files to source from modules
-module_files <- list.files(
-    path = "modules",
-    pattern = "\\.R$",
-    full.names = TRUE,
-    ignore.case = TRUE
-)
